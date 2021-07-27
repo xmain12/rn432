aa3fe928950071b45ff5237657c9038ee7ab6043
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-07-27 16:06:49 +0530

    ඔයාලට rpack එකෙත් made by වෙනස් කරන්න පුලුවන්

--- plugins/rwc.js
+++ plugins/rwc.js
@@ -16,7 +16,7 @@ if (Config.WORKTYPE == 'private') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/watercolour?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=RAVANA%20SL&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by RAVANA*' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
 
     }));
 }
@@ -29,7 +29,7 @@ else if (Config.WORKTYPE == 'public') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/watercolour?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=RAVANA%20SL&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by RAVANA*' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
 
     }));
     
