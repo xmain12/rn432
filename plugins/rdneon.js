4940d0a1ec9ee72c73cea7c96178933bfbf0a632
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-07-27 16:34:13 +0530

    ඔයාලට rpack එකෙත් made by වෙනස් කරන්න පුලුවන්

--- plugins/rdneon.js
+++ plugins/rdneon.js
@@ -1,7 +1,7 @@
 /* # Exclusively from RAVANA
 */
 
-const Asena = require('../events');
+const RAVANA = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
@@ -10,26 +10,26 @@ const need = "type some word after command\n🗡🗡"
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'rdneon ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+    RAVANA.addCommand({ pattern: 'rdneon ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
         var ttinullimage = await axios.get(`https://videfikri.com/api/textmaker/darkneon/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by RAVANA*' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
 
     }));
 }
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'rdneon ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+    RAVANA.addCommand({ pattern: 'rdneon ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
         var ttinullimage = await axios.get(`https://videfikri.com/api/textmaker/darkneon/?text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by RAVANA*' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
 
     }));
     
