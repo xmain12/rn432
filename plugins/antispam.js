180548abfa64d67dbb26430b03d30df531bd8e26
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-07-31 19:56:18 +0530

    ravana

--- plugins/antispam.js
+++ plugins/antispam.js
@@ -1,13 +1,13 @@
+/*RAVANA*/
+
 const RAVANA = require('../events');
-
 const {MessageType} = require('@adiwajshing/baileys');
-
 const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
 
 const code = ('𝙰\n𝙽\n𝚃\n𝙸\n𝕊\nℙ\n𝔸\nᴍ\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n*☮️RAVANA☮️*')
 const FINAL = "THIS IS AN ANTISAPM (anti lag),\nමෙම කේතය ඔබගේ දුරකථනය හදිසි ස්පෑම් එල්ල වීමකදි  සිරවිම වලකාලයි"
 const TMUTE = "Trying to close temporary,\nතාවකාලිකව සමුහය වැසීමට උත්සාහ කිරීම (මම 𝕒𝕕𝕞𝕚𝕟නම් වැහෙයි)"
-const MUT = ".mute 1d"
+const MUT = ".mute"
 const SCXR = "දැන් antispam එක ක්‍රියාත්මක වෙමින් පවතී...."
 RAVANA.addCommand({pattern: 'antispam', fromMe: true, deleteCommand: true, desc: Ln,}, (async (message, match) => {
 
