96e28b29f85c92e548023c945b7fa97cb41c5d76
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-08-02 20:37:06 +0530

    strong

--- plugins/antispam.js
+++ plugins/antispam.js
@@ -1,29 +1,41 @@
-/*RAVANA*/
+/*RAVANA BOT ANTISPAM*/
 
 const RAVANA = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
-const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
+const RP = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
 
-const code = ('𝙰\n𝙽\n𝚃\n𝙸\n𝕊\nℙ\n𝔸\nᴍ\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n*☮️RAVANA☮️*')
+const rcd = ('𝙰\n𝙽\n𝚃\n𝙸\n𝕊\nℙ\n𝔸\nᴍ\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n*☮️RAVANA☮️*')
 const FINAL = "THIS IS AN ANTISAPM (anti lag),\nමෙම කේතය ඔබගේ දුරකථනය හදිසි ස්පෑම් එල්ල වීමකදි  සිරවිම වලකාලයි"
-const TMUTE = "Trying to close temporary,\nතාවකාලිකව සමුහය වැසීමට උත්සාහ කිරීම (මම 𝕒𝕕𝕞𝕚𝕟නම් වැහෙයි)"
-const MUT = ".mute"
-const SCXR = "දැන් antispam එක ක්‍රියාත්මක වෙමින් පවතී...."
-RAVANA.addCommand({pattern: 'antispam', fromMe: true, deleteCommand: true, desc: Ln,}, (async (message, match) => {
+const RAVANAMUTE = "Trying to close temporary,\nතාවකාලිකව සමුහය වැසීමට උත්සාහ කිරීම (මම 𝕒𝕕𝕞𝕚𝕟නම් වැහෙයි)"
+const MUTE = ".mute"
+const RAVANA = "දැන් antispam එක ක්‍රියාත්මක වෙමින් පවතී...."
+RAVANA.addCommand({pattern: 'antispam', fromMe: true, deleteCommand: true, desc: RP,}, (async (message, match) => {
 
-  var msg = await message.reply('Preforming🛠....');
+  var msg = await message.reply('සැකසමින්🛠....');
 
   await message.client.sendMessage(
-    message.jid,TMUTE, MessageType.text);
+    message.jid,RAVANAMUTE, MessageType.text);
 
     await message.client.sendMessage(
-      message.jid,MUT, MessageType.text);
+      message.jid,MUTE, MessageType.text);
 
       await message.client.sendMessage(
-        message.jid,SCXR, MessageType.text);
+        message.jid,RAVANA, MessageType.text);
 
         await message.client.sendMessage(
-          message.jid,code, MessageType.text);
+          message.jid,rcd, MessageType.text);
+  
+              await message.client.sendMessage(
+                 message.jid,RAVANAMUTE, MessageType.text);
+
+               await message.client.sendMessage(
+               message.jid,MUTE, MessageType.text);
+
+      await message.client.sendMessage(
+        message.jid,RAVANA, MessageType.text);
+
+        await message.client.sendMessage(
+          message.jid,rcd, MessageType.text);
           
 
   }));
