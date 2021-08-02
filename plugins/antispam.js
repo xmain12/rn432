682956432e4a60d2855c7e793ccc37db3921fa63
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-08-02 21:14:37 +0530

    fix error

--- plugins/antispam.js
+++ plugins/antispam.js
@@ -11,7 +11,7 @@ const rcd = ('𝙰\n𝙽\n𝚃\n𝙸\n𝕊\nℙ\n𝔸\nᴍ\n\n\n\n\n\n\n\n\n\n\n
 const FINAL = "THIS IS AN ANTISAPM (anti lag),\nමෙම කේතය ඔබගේ දුරකථනය හදිසි ස්පෑම් එල්ල වීමකදි  සිරවිම වලකාලයි"
 const RAVANAMUTE = "Trying to close temporary,\nතාවකාලිකව සමුහය වැසීමට උත්සාහ කිරීම (මම 𝕒𝕕𝕞𝕚𝕟නම් වැහෙයි)"
 const MUTE = ".mute"
-const RAVANA = "දැන් antispam එක ක්‍රියාත්මක වෙමින් පවතී...."
+const aaa = "දැන් antispam එක ක්‍රියාත්මක වෙමින් පවතී...."
 RAVANA.addCommand({pattern: 'antispam', fromMe: true, deleteCommand: true, desc: RP,}, (async (message, match) => {
 
   var msg = await message.reply('සැකසමින්🛠....');
@@ -23,7 +23,7 @@ RAVANA.addCommand({pattern: 'antispam', fromMe: true, deleteCommand: true, desc:
       message.jid,MUTE, MessageType.text);
 
       await message.client.sendMessage(
-        message.jid,RAVANA, MessageType.text);
+        message.jid,aaa, MessageType.text);
 
         await message.client.sendMessage(
           message.jid,rcd, MessageType.text);
@@ -35,7 +35,7 @@ RAVANA.addCommand({pattern: 'antispam', fromMe: true, deleteCommand: true, desc:
                message.jid,MUTE, MessageType.text);
 
       await message.client.sendMessage(
-        message.jid,RAVANA, MessageType.text);
+        message.jid,aaa, MessageType.text);
 
         await message.client.sendMessage(
           message.jid,rcd, MessageType.text);
