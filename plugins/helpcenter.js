41bcd58d4608200c1973e635117e9fbf8755054a
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-08-03 11:33:26 +0530

    Add 🇱🇰RAVANA HELP CE

---
+++ plugins/helpcenter.js
@@ -0,0 +1,45 @@
+/* # Exclusively from RAVANA
+# Do not use this fore any commercial thing
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved RAVANA Wa.me/+94714898434
+# Credit : usufusta-whatsappAsena
+*/
+
+const RAVANA  = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+RAVANA.addCommand({pattern: 'rhelp', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*🇱🇰HELLOW THIS IS RAVANA HELP CENTER🇱🇰* \n\n✨What's Your Problem \n\n\n▶සිංදු වීඩියෝ එන්නේ නෑ(.1) \n\n▶ලොගෝ වැඩ නෑ(.2)\n\n▶Alive එක වැඩ කරන්නේ නෑ(.3)\n\n▶Shutdown කරාම on කරන්නේ කොහොමද(.4) \n\n▶බොට් එක පාරම වැඩ නැතුව ගියොත් මොකද කරන්නේ(.5) \n\n▶වෙනත්(.6) \n\n\n🥰Thank you for using RAVANA HELP CENTER🇱🇰");
+
+}));
+RAVANA.addCommand({pattern: '1', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*කරුණාකර ඔබගේ බොට්ව restart කරන්න.* \n\n .restart \n\n🥰Thank you for using RAVANA HELP CENTER🇱🇰");
+
+}));
+RAVANA.addCommand({pattern: '2', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*එය අපගේ ප්‍රශ්නයක් නොවේ එය ලොගෝ සයිට් එකේ ලිමිට් එක නිසා* \n\nදැන් එයට විසදුමක් ඇත .\n\n```ඔයාලට පුලුවන් ඒ ලොගෝ api වෙනස් කිරීමට ```\n\n.setvar XTEAM_API:ඔයාල ගත්ත api එක \n\n .setvar LOLUHUMAN_API:ඔයාල ගත්ත api එක \n\n\nඔය api ගන්න ක්‍රමේ මෙම වීඩියෝ එකේ ඇත.\n https://youtu.be/vGHpome5e3k\n\n\n 🥰Thank you for using RAVANA HELP CENTER🇱🇰");
+
+}));
+RAVANA.addCommand({pattern: '3', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*ඔබේ alive එක වැඩ නොකර නේ ඔබ alive logo එක වෙනස් කර ඒ ලින්ක් එක වැරදි*\n\nහරියට ලින්ක් එක ගන්න\nසයිට් එක telegra.ph\n\n 🥰Thank you for using RAVANA HELP CENTER🇱🇰");
+
+}));
+RAVANA.addCommand({pattern: '4', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*ඔබ බොට්ව shutdown කරවිට ඔබ heroku එකට ගොස් නැවතත් Resources key එක on කර යුතුයි.*\n\n🥰Thank you for using RAVANA HELP CENTER🇱🇰");
+
+}));
+RAVANA.addCommand({pattern: '5', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*ඒකට කරන්න තියෙන්නේ ඔයා ආයේ heroku එකට ගිහින් more එක ඔබලා restart all dyno ඔබන්න.වෙනත් ප්‍රශ්නයක්නම් හෙරොකු එකේ log එකේ ss එකක් අපේ suppoted group එකට දාන්න.*\n\n🥰Thank you for using RAVANA HELP CENTER🇱🇰");
+
+}));
+RAVANA.addCommand({pattern: '6', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("*ප්‍රශ්නය අපගේ whatsapp supposed group https://chat.whatsapp.com/BZxwFPt5pUe19Y4EW9DjDP එක තුලට හෝ මෙම නම්බර් එකට දාන්න wa.me/94756872976*\n\n🥰Thank you for using RAVANA HELP CENTER🇱🇰");
+
+}));
