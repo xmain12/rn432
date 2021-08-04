5594748364fb04b0cc9f5378bf0864bb97eda821
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-08-04 18:00:19 +0530

    Try to fix tiktok error

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -128,7 +128,7 @@ else if (cn.WORKTYPE == 'public') {
         await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text)
 
         await axios
-          .get(`https://api.xteam.xyz/dl/tiktok?url=${userName}&APIKEY=5bd33b276d41d6b4`)
+          .get(`https://api.xteam.xyz/dl/tiktok?url=${userName}&APIKEY=${Config.XTEAM_API}`)
           .then(async (response) => {
             const {
               server_1,
@@ -139,7 +139,7 @@ else if (cn.WORKTYPE == 'public') {
             })
 
             await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-              caption: 'musicly id tiktok downloader\n ' +Config.CAPTION_KEY+ '| ©2021',
+              caption: 'musicly id tiktok downloader\n RAVANA| ©2021🇱🇰',
             })
           })
           .catch(
