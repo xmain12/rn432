23a8db0b3666a8f6473a721ea34a36d9dbe87dc3
RaOne <87294221+Agent34567@users.noreply.github.com>
2021-08-14 11:41:05 +0530

    ⚜RAVANA 3.5⚜
    💌RAVANA NEW UPDATE💌
    
    ⭕⭕⭕New version 3.5⭕⭕⭕
    
    💥add new logo pack .upack
    
    💥add error fixed bot
    
    💥add meme pack
    
    💥add new ai bot
    
    💥bug fixed 
    
    💥you can change downloading song , uplaoding song
    
    💥add auto bio
    
    💥add anti link 
    
    🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹

--- bot.js
+++ bot.js
@@ -155,7 +155,7 @@ async function whatsAsena () {
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('LK', { timeZone: 'Asia/Colombo' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\n💢 RAVANA'
+                const biography = '📅දිනය ' + utch + '\n⌚වේලාව ' + ov_time +'\n\n💢 RAVANA'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('351')) { // Portugal
